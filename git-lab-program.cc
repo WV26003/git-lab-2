d8655a2b3146f2fbee790ca0231251148c2eeb1a
Norman Lee <nl789118@ohio.edu>
2020-01-24 17:24:49 -0500

    New File

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Norman Lee
+ *        Date: 1/24/20
+ * Description: Program to test out uploading a C++ file to github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
